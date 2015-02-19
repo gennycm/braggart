d37eafd9004415ffdbb512df0a3eaeabe44a0850
gennycm <gennycm13@gmail.com>
2015-02-19 16:50:10 -0600

    Cambio de la pagina a parallax

--- index.php
+++ index.php
@@ -1,41 +1,81 @@
 <?php include_once("header.html");?>
 <!--BODY-->
 
-<div id="fullpage">
-    <div class="section">
-    	<div id="full-width-slider" class="royalSlider heroSlider rsMinW" style="width:100%;">
-		  <div class="rsContent">
-		    <img class="rsImg" src="img_product/img_1.png" alt="">
-		  </div>
-		  <div class="rsContent">
-		    <img class="rsImg" src="img_product/img_2.jpg" alt="">
-		  </div>
-		 <div class="rsContent">
-		    <img class="rsImg" src="img_product/img_3.jpg" alt="">
-		  </div>
-		  <div class="rsContent">
-		    <img class="rsImg" src="img_product/img_4.jpg" alt="">
-		  </div>
-		</div>
-		<a href="#" style="display:block" onclick="display_menu()">
+<div id="pagepiling">
+	    <div class="background_black"></div>
+	    <div class="section" id="home">
+	    	<div id="full-width-slider" class="royalSlider heroSlider rsMinW" style="width:100%;">
+			  <div class="rsContent">
+			    <img class="rsImg" src="img_product/img_1.png" alt="">
+			  </div>
+			  <div class="rsContent">
+			    <img class="rsImg" src="img_product/img_2.jpg" alt="">
+			  </div>
+			 <div class="rsContent">
+			    <img class="rsImg" src="img_product/img_3.jpg" alt="">
+			  </div>
+			  <div class="rsContent">
+			    <img class="rsImg" src="img_product/img_4.jpg" alt="">
+			  </div>
+			</div>
+			<a href="#" style="display:block" onclick="display_menu()">
 			<div class="menu-toggle"></div>
 			<div class="text_toggle">
 				<h5>MENÚ</h5>
 			</div>
-		</a>
-    </div>
-    <div class="section us">
+		</a>	    	
+	    </div>
+	    <div class="background_black"></div>
+	    <div class="section" id="us">
+	    		<h1>La tienda</h1>
+	    		<p></p>
+	    </div>
+	    <div class="background_black"></div>
+	    <div class="section" id="shirts">
+	    		<h1>Camisas</h1>
+	    		<p></p>
+	    </div>
     	<div class="background_black"></div>
-    	<h1 class="main_title">LA TIENDA</h1>
-    </div>
-    <div class="section shirts">
-    	<div class="background_black"></div>
-    	<h1 class="main_title">CAMISAS</h1>
-    </div>
-    <div class="section contact"> 
-    	<div class="background_black"></div>
-    </div>
-</div>
+	    <div class="section" id="contact">
+	    	<div class="row">
+                    <div class="col-lg-6 col-md-6 col-sm-6">
+                    </div>
+                   <div class="col-lg-6 col-md-6 col-sm-6">
+	                   	<div>
+	                   		<table class="contact">
+	                   			<tr>
+	                   				<td colspan="2" class="decoration">
+	                   					/+-'"-+?
+	                   				</td>
+	                   			</tr>
+	                   			<tr>
+	                   				<td class="icon"><i class="fa fa-phone fa-lg"></i></td>
+	                   				<td class="text">(999) 9 48 30 46 </td>
+	                   			</tr>
+	                   			<tr>
+	                   				<td class="icon"><i class="fa fa-envelope fa-lg"></i></td>
+	                   				<td class="text">contacto@braggart.com </td>
+	                   			</tr>
+	                   			<tr>
+	                   				<td class="icon"><i class="fa fa-facebook-official fa-lg"></i></td>
+	                   				<td class="text">Tienda Braggart</td>
+	                   			</tr>
+	                   			<tr>
+	                   				<td class="icon"><i class="fa fa-insta-official fa-lg"></i></td>
+	                   				<td class="text">@BraggartMX </td>
+	                   			</tr>
+	                   			<tr>
+	                   				<td colspan="2" class="decoration">
+	                   					U+-'"-+u
+	                   				</td>
+	                   			</tr>	
+	                   		</table>
+	                   	</div>
+                    </div>
+            </div>
+	    </div>
+	</div>
+</div><!-- /.container-fluid -->
 
 <!--BODY-->
 <?php include_once("footer.html");?>
