8b049dc82796d4e2d5486c074f16e20a181fef46
bheftye <bheftye92@gmail.com>
2015-02-19 19:22:27 -0600

    Shorts, y cambios en el menú

--- index.php
+++ index.php
@@ -2,85 +2,174 @@
 <!--BODY-->
 
 <div id="pagepiling">
-		<a href="#" style="display:block; position:fixed;z-index:1000;" onclick="display_menu()">
-			<div class="menu-toggle"></div>
-			<div class="text_toggle">
-				<h5>MENÚ</h5>
-			</div>
-		</a>	   
-	   
-	    <div class="section" id="home">
-	    	 <div class="background_black"></div>
-	    	<div id="full-width-slider" class="royalSlider heroSlider rsMinW" style="width:100%;">
-			  <div class="rsContent">
-			    <img class="rsImg" src="img_product/img_1.png" alt="">
-			  </div>
-			  <div class="rsContent">
-			    <img class="rsImg" src="img_product/img_2.jpg" alt="">
-			  </div>
-			 <div class="rsContent">
-			    <img class="rsImg" src="img_product/img_3.jpg" alt="">
-			  </div>
-			  <div class="rsContent">
-			    <img class="rsImg" src="img_product/img_4.jpg" alt="">
-			  </div>
-			</div>
-			 	
-	    </div>
-	    
-	    <div class="section" id="us">
-	    	<div class="background_black"></div>
-    		<h1>LA TIENDA</h1>
-    		<p></p>
-	    </div>
-	    
-	    <div class="section" id="shirts">
-	    	<div class="background_black"></div>
-    		<h1>CAMISAS</h1>
-    		<p></p>
-	    </div>
-    	
-	    <div class="section" id="contact">
-	    	<div class="background_black"></div>
-	    	<div class="row">
+        <a href="#" style="display:block; position:fixed;z-index:1000;" onclick="display_menu()">
+            <div class="menu-toggle"></div>
+            <div class="text_toggle">
+                <h5>MENÚ</h5>
+            </div>
+        </a>       
+       
+        <div class="section" id="home">
+            <div class="background_black"></div>
+            <div id="full-width-slider" class="royalSlider heroSlider rsMinW" style="width:100%;">
+              <div class="rsContent">
+                <img class="rsImg" src="img_product/img_1.png" alt="">
+              </div>
+              <div class="rsContent">
+                <img class="rsImg" src="img_product/img_2.jpg" alt="">
+              </div>
+             <div class="rsContent">
+                <img class="rsImg" src="img_product/img_3.jpg" alt="">
+              </div>
+              <div class="rsContent">
+                <img class="rsImg" src="img_product/img_4.jpg" alt="">
+              </div>
+            </div>
+                
+        </div>
+        
+        <div class="section" id="us">
+            <div class="background_black"></div>
+            <h1>LA TIENDA</h1>
+            <p></p>
+        </div>
+        
+        <div class="section" id="shirts">
+            <div class="background_black"></div>
+            <h1>CAMISAS</h1>
+            <p></p>
+        </div>
+        
+        <div class="section" id="contact">
+            <div class="background_black"></div>
+            <div class="row">
                     <div class="col-lg-6 col-md-6 col-sm-6">
                     </div>
                    <div class="col-lg-6 col-md-6 col-sm-6">
-	                   	<div>
-	                   		<table class="contact">
-	                   			<tr>
-	                   				<td colspan="2" class="decoration">
-	                   					/+-'"-+?
-	                   				</td>
-	                   			</tr>
-	                   			<tr>
-	                   				<td class="icon"><i class="fa fa-phone fa-lg"></i></td>
-	                   				<td class="text">(999) 9 48 30 46 </td>
-	                   			</tr>
-	                   			<tr>
-	                   				<td class="icon"><i class="fa fa-envelope fa-lg"></i></td>
-	                   				<td class="text">contacto@braggart.com </td>
-	                   			</tr>
-	                   			<tr>
-	                   				<td class="icon"><i class="fa fa-facebook-official fa-lg"></i></td>
-	                   				<td class="text">Tienda Braggart</td>
-	                   			</tr>
-	                   			<tr>
-	                   				<td class="icon"><i class="fa fa-insta-official fa-lg"></i></td>
-	                   				<td class="text">@BraggartMX </td>
-	                   			</tr>
-	                   			<tr>
-	                   				<td colspan="2" class="decoration">
-	                   					U+-'"-+u
-	                   				</td>
-	                   			</tr>	
-	                   		</table>
-	                   	</div>
+                        <div>
+                            <table class="contact">
+                                <tr>
+                                    <td colspan="2" class="decoration">
+                                        /+-'"-+?
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="icon"><i class="fa fa-phone fa-lg"></i></td>
+                                    <td class="text">(999) 9 48 30 46 </td>
+                                </tr>
+                                <tr>
+                                    <td class="icon"><i class="fa fa-envelope fa-lg"></i></td>
+                                    <td class="text">contacto@braggart.com </td>
+                                </tr>
+                                <tr>
+                                    <td class="icon"><i class="fa fa-facebook-official fa-lg"></i></td>
+                                    <td class="text">Tienda Braggart</td>
+                                </tr>
+                                <tr>
+                                    <td class="icon"><i class="fa fa-insta-official fa-lg"></i></td>
+                                    <td class="text">@BraggartMX </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2" class="decoration">
+                                        U+-'"-+u
+                                    </td>
+                                </tr>   
+                            </table>
+                        </div>
                     </div>
             </div>
-	    </div>
-	</div>
+        </div>
+    </div>
 </div><!-- /.container-fluid -->
 
 <!--BODY-->
 <?php include_once("footer.html");?>
+<script>
+    /*Parallax scrolling*/
+        var deleteLog = false;
+        $(document).ready(function() {
+            $('#pagepiling').pagepiling({
+                menu: false,
+                anchors: ['home', 'shirts', 'us', 'contact'],
+                navigation: {
+                    'textColor': '#f2f2f2',
+                    'bulletsColor': '#ccc',
+                    'position': 'right',
+                    'tooltips': ['INICIO', 'CAMISAS', 'LA TIENDA', 'CONTACTO']
+                }
+
+            });
+        });
+
+    jQuery(document).ready(function($) {
+      $('#full-width-slider').royalSlider({
+        loop:true,
+        keyboardNavEnabled: true,
+        imageScaleMode: "fill",
+        controlNavigation: "none",
+        navigateByClick: true,
+        usePreloader: true,
+        sliderDrag: false
+      });
+
+      var width = $(window).width();
+      var height = $(window).height();
+
+      $('#full-width-slider').css({width: width+"px", height:height+"px"})
+
+    });
+
+    /*$(document).ready(function() {
+        $('#fullpage').fullpage({
+        //Navigation
+        menu: false,
+        anchors:['firstSlide', 'secondSlide'],
+        navigation: true,
+        navigationPosition: 'right',
+        navigationTooltips: ['Slide', 'La Tienda', 'Camisas', 'Contacto'],
+        slidesNavigation: false,
+        slidesNavPosition: 'bottom',
+
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 700,
+        autoScrolling: true,
+        scrollBar: false,
+        easing: 'easeInQuart',
+        easingcss3: 'ease',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: false,
+        normalScrollElements: '#element1, .element2',
+        scrollOverflow: false,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+        recordHistory: true,
+
+        //Design
+        controlArrows: true,
+        verticalCentered: true,
+        resize : true,
+        sectionsColor : ['#ccc', '#fff'],
+        fixedElements: '#header, .footer',
+        responsive: 0,
+
+        //Custom selectors
+        sectionSelector: '.section',
+        slideSelector: '.slide',
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+        afterResize: function(){},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction){}
+    });
+    });*/
+    </script>
--- shirts.php
+++ shirts.php
@@ -1,19 +1,123 @@
 <?php include_once("header.html");?>
 <!--BODY-->
 
-<div id="fullpage">
-    <div class="section shirt">
-    	<div class="background_black"></div>
-    	<h1 class="main_title">LA TIENDA</h1>
+<div id="pagepiling">
+        <a href="#" style="display:block; position:fixed;z-index:1000;" onclick="display_menu()">
+            <div class="menu-toggle"></div>
+            <div class="text_toggle">
+                <h5>MENÚ</h5>
+            </div>
+        </a>       
+       
+        <div class="section shirt" id="shirt1" style="background-image:url(./img_product/img_4.jpg);">
+            <div class="background_black"></div>
+        </div>
+        
+        <div class="section shirt" id="shirt2" style="background-image:url(./img_product/img_6.jpg);">
+            <div class="background_black"></div>
+            
+        </div>
+        
+        <div class="section shirt" id="shirt3" style="background-image:url(./img_product/img_3.jpg);">
+            <div class="background_black"></div>
+            
+        </div>
+         
+        <div class="section shirt" id="shirt4" style="background-image:url(./img_product/img_5.jpg);">
+            <div class="background_black"></div>
+           
+        </div>
     </div>
-    <div class="section shirt">
-    	<div class="background_black"></div>
-    	<h1 class="main_title">CAMISAS</h1>
-    </div>
-    <div class="section shirt"> 
-    	<div class="background_black"></div>
-    </div>
-</div>
+</div><!-- /.container-fluid -->
 
 <!--BODY-->
 <?php include_once("footer.html");?>
+<script>
+    /*Parallax scrolling*/
+        var deleteLog = false;
+        $(document).ready(function() {
+            $('#pagepiling').pagepiling({
+                menu: false,
+                anchors: ['elegante-ajustada', 'business-ajustada', 'office-ajustada', 'casual-ajustada'],
+                navigation: {
+                    'textColor': '#f2f2f2',
+                    'bulletsColor': '#ccc',
+                    'position': 'right',
+                    'tooltips': ['ELEGANTE AJUSTADA', 'BUSINESS AJUSTADA', 'OFFICE AJUSTADA', 'CASUAL AJUSTADA']
+                }
+
+            });
+        });
+
+    jQuery(document).ready(function($) {
+      $('#full-width-slider').royalSlider({
+        loop:true,
+        keyboardNavEnabled: true,
+        imageScaleMode: "fill",
+        controlNavigation: "none",
+        navigateByClick: true,
+        usePreloader: true,
+        sliderDrag: false
+      });
+
+      var width = $(window).width();
+      var height = $(window).height();
+
+      $('#full-width-slider').css({width: width+"px", height:height+"px"})
+
+    });
+
+    /*$(document).ready(function() {
+        $('#fullpage').fullpage({
+        //Navigation
+        menu: false,
+        anchors:['firstSlide', 'secondSlide'],
+        navigation: true,
+        navigationPosition: 'right',
+        navigationTooltips: ['Slide', 'La Tienda', 'Camisas', 'Contacto'],
+        slidesNavigation: false,
+        slidesNavPosition: 'bottom',
+
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 700,
+        autoScrolling: true,
+        scrollBar: false,
+        easing: 'easeInQuart',
+        easingcss3: 'ease',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: false,
+        normalScrollElements: '#element1, .element2',
+        scrollOverflow: false,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+        recordHistory: true,
+
+        //Design
+        controlArrows: true,
+        verticalCentered: true,
+        resize : true,
+        sectionsColor : ['#ccc', '#fff'],
+        fixedElements: '#header, .footer',
+        responsive: 0,
+
+        //Custom selectors
+        sectionSelector: '.section',
+        slideSelector: '.slide',
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+        afterResize: function(){},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction){}
+    });
+    });*/
+    </script>
